33c48c8ca38524731db85eca34b2be436ca4cc11
Arib <aribchowdhury03@gmail.com>
2022-02-20 23:41:54 -0500

    updateValidMoves for bishop, queen, rook

--- src/chess-assets/bishop.py
+++ src/chess-assets/bishop.py
@@ -4,4 +4,19 @@ class Bishop(ChessPiece):
     def __init__(self):
         pass
 
+    def updateValidMoves(self, board: dict, king_pos: str):
+        self.valid_moves = set()
+        for piece_name, shift in [[1,1],[1,-1],[-1,1],[-1,-1]]:
+            temp_loc = self._addToLocWithNums(self.loc, shift)
+            while temp_loc != "NA":
+                switch = self.board.get(temp_loc, None)
+                if switch == None:
+                    self.valid_moves.add(temp_loc)
+                    temp_loc = self._addToLocWithNums(temp_loc, shift)
+                elif switch.getName() == self.getOtherColor() + piece_name:
+                    self.valid_moves.add(temp_loc)
+                    break
+                else:
+                    break
+        return super().updateValidMoves(board, king_pos)
 # Overload updateValidMoves
--- src/chess-assets/queen.py
+++ src/chess-assets/queen.py
@@ -3,5 +3,20 @@ from .piece import ChessPiece
 class Queen(ChessPiece):
     def __init__(self):
         pass
-
+    
+    def updateValidMoves(self, board: dict, king_pos: str):
+        self.valid_moves = set()
+        for piece_name, shift in [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]:
+            temp_loc = self._addToLocWithNums(self.loc, shift)
+            while temp_loc != "NA":
+                switch = self.board.get(temp_loc, None)
+                if switch == None:
+                    self.valid_moves.add(temp_loc)
+                    temp_loc = self._addToLocWithNums(temp_loc, shift)
+                elif switch.getName() == self.getOtherColor() + piece_name:
+                    self.valid_moves.add(temp_loc)
+                    break
+                else:
+                    break
+        return super().updateValidMoves(board, king_pos)
 # Overload updateValidMoves
